0ab805c0f1c7825e0f7c57e9269a30664fb0ecb1
Andrey-GitHub-Lab <108327119+Andrey-GitHub-Lab@users.noreply.github.com>
2022-06-29 16:46:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
         $('.header__burger,.header__menu').toggleClass('active')
         $('body').toggleClass('lock')
     })
+    $('.block__title-1').click(function(event){
+        $('.block__text-1').toggleClass('active')
+    })
+    $('.block__title-2').click(function(event){
+        $('.block__text-2').toggleClass('active')
+    })
+    $('.block__title-3').click(function(event){
+        $('.block__text-3').toggleClass('active')
+    })
 })
